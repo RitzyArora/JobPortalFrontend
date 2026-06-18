8a9b6f120f01beac334c34e3e838ab00b176fe6e
Ritika Arora <ritikaaroramcageu@gmail.com>
2026-06-18 16:05:40 +0530

    fixed backend path again

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API=axios.create({baseURL:"https://jobportalbackend-production-aa98.up.railway.app"})
+const API=axios.create({baseURL:"https://jobportalbackend-production-aa98.up.railway.app/api"})
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
     if(token)
